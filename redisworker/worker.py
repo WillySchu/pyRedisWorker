e6c815ef5d587b3cf384266fd7b4715bfbd94a19
Will Schumacher <meintodengel@gmail.com>
2017-02-28 14:56:59 -0800

    update gitignore

--- redisworker/worker.py
+++ redisworker/worker.py
@@ -1,11 +1,15 @@
 import redis
 import json
 import threading
+import traceback
 from Queue import Queue
 
 class Worker(object):
-    def __init__(self, namespace, host='localhost', port=6379, db=0, semaphoreKey=False, semaphoreTTL=60):
+    def __init__(self, namespace, callback, host='localhost', port=6379, db=0, semaphoreKey=False, semaphoreTTL=60, trace=True):
         self.namespace = namespace
+        self.callback = callback
+        self.trace = trace
+
         self.r = redis.StrictRedis(host=host, port=port, db=db)
         self.p = redis.StrictRedis(host=host, port=port, db=db)
         self.q = Queue()
@@ -19,7 +23,30 @@ class Worker(object):
         while True:
             o = self.r.brpop(self.namespace, timeout=60)
             if o is not None:
-                print o
+                print 'Received: %s' % data[0]
+                envelope = json.loads(data[1])
+                t = Thread(self.run, envelope)
+                t.start()
+                self.pub()
+
+    def run(self, data):
+        res = {}
+        res['request'] = data
+        try:
+            res['result'] = self.callback(data)
+        except:
+            if self.trace:
+                err = traceback.format_exec()
+                res['error'] = err
+
+            res['result'] = {}
+
+        self.q.put(json.dumps(res))
+
+    def pub(self):
+        s = self.q.get()
+        print s
+        print 'publish'
 
 class Thread(threading.Thread):
     def __init__(self, callback, data):
@@ -27,5 +54,5 @@ class Thread(threading.Thread):
         self.callback = callback
         self.data = data
 
-        def run(self):
-            self.callback(self.data)
+    def run(self):
+        self.callback(self.data)
