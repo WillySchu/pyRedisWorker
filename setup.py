91f456cd28d1c1fef5b45ffb5f974f88dbb82bf1
Will Schumacher <meintodengel@gmail.com>
2017-03-01 15:50:54 -0800

    version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='redisworker',
-    version='0.1',
+    version='0.2',
     description='Communicate jobs through redis',
     url='https://github.com/metricstory/pyredisworker',
-    download_url='https://github.com/metricstory/redisworker/archive/0.1.tar.gz',
+    download_url='https://github.com/metricstory/redisworker/archive/0.2.tar.gz',
     author='Will Schumacher',
     author_email='will@metricstory.com',
     license='MIT',
